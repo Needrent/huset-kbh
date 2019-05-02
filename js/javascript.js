4039b8df13be14b31db82fd68581a75f2c15ec87
SEJ <sej@needrent.dk>
2019-05-02 09:15:05 +0200

    JS, Logo back button

--- js/javascript.js
+++ js/javascript.js
@@ -1,1 +1,10 @@
+const logo = document.querySelector("#logo");
+
+logo.addEventListener("click", menu);
+logo.style.cursor = "pointer";
+
+function menu(){
+    window.location.href = "index.html";
+}
+
 console.log("Hello World");
