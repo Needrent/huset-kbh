90a1b7c30b983d65cfe314a72cdd400eb571319c
SEJ <sej@needrent.dk>
2019-05-06 11:18:48 +0200

    broken

--- js/index.js
+++ js/index.js
@@ -8,13 +8,13 @@ function menu(){
     window.location.href = "index.html";
 }
 /*------------------------------------------------------------Menu */
- const template = document.querySelector("template").content;
+ const template = document.querySelector("tempContent").content;
         const parent = document.querySelector("main");
         const urlParms = new URLSearchParams(window.location.search);
         const catID = urlParms.get("cat");
         const catNav = document.querySelector("#catNav");
-        const baseLink = "http://keawp.needrent.dk/wp-json/wp/v2/db_huset";
 
+/*
         function loadCats() {
             fetch("http://keawp.needrent.dk/wp-json/wp/v2/huset_genre?per_page=15").then(e => e.json()).then(buildCatMenu);
         }
@@ -49,65 +49,18 @@ function menu(){
                 clone.querySelector("a").href = "details.html?id=" + event.id;
                 parent.appendChild(clone);
             })
-        }
-
-/*const eventName = clone.querySelector("h3");
-        const img = clone.querySelector(".imgContainer img");
-        const eventShort = clone.querySelector("section article");
-        const eventMore = clone.querySelector("a");*/
+        }/*
 
 
-/*const template = document.querySelector("#tempContent").content;
-        const parent = document.querySelector("main");
-        const urlParms = new URLSearchParams(window.location.search);
-        const catID = urlParms.get("cat");
-        const catNav = document.querySelector("#catNav");
-        const baseLink = "http://keawp.needrent.dk/wp-json/wp/v2/";
-        function loadCats() {
-            fetch(baseLink + "huset_genre?per_page=15").then(e => e.json()).then(buildCatMenu);
-        }
-        function loadByCat(cat) {
-            fetch(baseLink + "event?huset_genre=" + cat + "&_embed").then(e => e.json()).then(show);
-        }
-        function loadAll() {
-            fetch(baseLink + "event?_embed").then(e => e.json()).then(show);
-        }
-        loadCats()
-        if (catID) {
-            loadByCat(catID)
-        } else {
-            loadAll()
-        }
-        function buildCatMenu(cats) {
-            cats.forEach(cat => {
-                const newA = document.createElement("a");
-                const newS = document.createElement("span");
-                newA.textContent = cat.name;
-                newA.href = "?cat=" + cat.id;
-                catNav.appendChild(newA);
-                console.log(cat.name + cat.id);
-            })
-        }
-       function show(events) {
-            events.forEach(event => {
-// clone the template
-        const clone = template.cloneNode(true);
-
-        // populate the template
-        const eventName = clone.querySelector("h3");
-        const img = clone.querySelector(".imgContainer img");
-        const eventShort = clone.querySelector("section article");
-        const eventMore = clone.querySelector("a");
-            })
-        }*/
 /*------------------------------------------------------------Index*/
 //const frontPage = "db_huset?_embed";
+const baseLink = "http://keawp.needrent.dk/wp-json/wp/v2/db_huset?_embed";
 
-/*function loadData(link){
+function loadData(link){
     fetch(link).then(e=>e.json()).then(data=>show(data));
-}*/
+}
 
-/*function show(data){
+function show(data){
     data.forEach(event=>{
         console.log(event);
         // clone the template
@@ -142,7 +95,5 @@ function menu(){
         // append to the DOM
         parent.appendChild(clone);
     })
-}*/
-/*console.log(loadAll());
-loadAll();*/
-//loadData(baseLink+frontPage);
+    LoadData(baseLink);
+}
